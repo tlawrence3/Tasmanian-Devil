944aca0104ab472dfe8d3d4f0808e7d556c3d1fd
Travis Lawrence <tlawrence3@ucmerced.edi>
2016-07-19 15:35:13 -0700

    user interface layout finished

--- command_interface.py
+++ command_interface.py
@@ -5,6 +5,10 @@ subparsers = parser.add_subparsers(help="sub-command help")
 
 # gene subcommand parser
 parser_gene = subparsers.add_parser("gene", help="need to write")
+
+parser_gene.add_argument("model", type=argparse.FileType("r"), help="need to write")
+parser_gene.add_argument("expression_set", type=argparse.FileType("r"), help="need to write")
+parser_gene.add_argument("-o", "--output", type=argparse.FileType("w"), help="need to write")
 parser_gene.add_argument("-u", "--upper", type=float, default=0.2,
                          help="high help")
 parser_gene.add_argument("-l", "--lower", type=float, default=0.2,
@@ -18,6 +22,8 @@ gene_group.add_argument("-s", "--sbml", action="store_true",
 # model subcommand parser
 parser_model = subparsers.add_parser("model", help="need to write")
 model_group = parser_model.add_mutually_exclusive_group(required=True)
+parser_model.add_argument("model", type=argparse.FileType("r"), help="need to write")
+parser_model.add_argument("-o", "--output", type=argparse.FileType("w"), help="need to write")
 model_group.add_argument("-c", "--cobra", action="store_true",
                          help="cobra help")
 model_group.add_argument("-s", "--sbml", action="store_true",
@@ -39,11 +45,13 @@ parser_model.add_argument("--metabolite2carbon", action="store_true", help="carb
 # flux subcommand parser
 parser_flux = subparsers.add_parser("flux", help="need to write")
 flux_group = parser_flux.add_mutually_exclusive_group(required=True)
+parser_flux.add_argument("model", type=argparse.FileType("r"), help="need to write")
+parser_flux.add_argument("-o", "--output", type=str, help="need to write")
 flux_group.add_argument("-c", "--cobra", action="store_true",
                          help="cobra help")
 flux_group.add_argument("-s", "--sbml", action="store_true",
                          help="sbml help")
-parser_flux.add_argument("-b", "biomass-rxn", type=str, help="biomass help")
+parser_flux.add_argument("-b", "--biomass-rxn", type=str, help="biomass help")
 parser_flux.add_argument("-a", type=float, help="a")
 
 #parse args
