d829cdf6c343157ec2c7bc7e982056152fdbfae4
Travis Lawrence <tlawrence3@ucmerced.edu>
2017-08-08 16:51:12 -0700

    finished gene subcommand

--- EXAMOARC/build/lib/EXAMOARC/EXAMOARC.py
+++ EXAMOARC/build/lib/EXAMOARC/EXAMOARC.py
@@ -1,4 +1,15 @@
+from __future__ import print_function
 import argparse
+import gene as gene_class
+
+def gene(args):
+    gene_class.gene_classify(args.expression_set, args.upper, args.lower, args.output)
+
+def model(args):
+    print("model subcommand")
+
+def flux(args):
+    print("flux subcommand")
 
 def main():
     parser = argparse.ArgumentParser(prog="EXAMO-ARC")
@@ -7,18 +18,14 @@ def main():
     # gene subcommand parser
     parser_gene = subparsers.add_parser("gene", help="need to write")
     
-    parser_gene.add_argument("model", type=argparse.FileType("r"), help="need to write")
     parser_gene.add_argument("expression_set", type=argparse.FileType("r"), help="need to write")
-    parser_gene.add_argument("-o", "--output", type=argparse.FileType("w"), help="need to write")
-    parser_gene.add_argument("-u", "--upper", type=float, default=0.2,
+    parser_gene.add_argument("-o", "--output", type=argparse.FileType("w"), 
+                             default="gene_classification.txt", help="need to write")
+    parser_gene.add_argument("-u", "--upper", type=float, default=0.75,
                              help="high help")
     parser_gene.add_argument("-l", "--lower", type=float, default=0.2,
                              help="low help")
-    gene_group = parser_gene.add_mutually_exclusive_group(required=True)
-    gene_group.add_argument("-c", "--cobra", action="store_true",
-                            help="cobra help")
-    gene_group.add_argument("-s", "--sbml", action="store_true",
-                            help="sbml help")
+    parser_gene.set_defaults(func=gene)
     
     # model subcommand parser
     parser_model = subparsers.add_parser("model", help="need to write")
@@ -42,6 +49,7 @@ def main():
     parser_model.add_argument("--nucleotide-conversions", type=argparse.FileType("r"),
                               help="nuc help")
     parser_model.add_argument("--metabolite2carbon", action="store_true", help="carbon help")
+    parser_model.set_defaults(func=model)
     
     # flux subcommand parser
     parser_flux = subparsers.add_parser("flux", help="need to write")
@@ -54,6 +62,8 @@ def main():
                              help="sbml help")
     parser_flux.add_argument("-b", "--biomass-rxn", type=str, help="biomass help")
     parser_flux.add_argument("-a", type=float, help="a")
+    parser_flux.set_defaults(func=flux)
     
     #parse args
     args = parser.parse_args()
+    args.func(args)
