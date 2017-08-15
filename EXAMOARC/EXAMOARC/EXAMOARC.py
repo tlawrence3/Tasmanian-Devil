5cfbb54e56c2c213c96f97aa310fbf77d6ea934a
egibb <egibb@ucmerced.edu>
2017-08-15 12:29:28 -0700

    tested set_parameters; now working

--- EXAMOARC/EXAMOARC/EXAMOARC.py
+++ EXAMOARC/EXAMOARC/EXAMOARC.py
@@ -7,7 +7,7 @@ def gene(args):
     gene_class.gene_classify(args.expression_set, args.upper, args.lower, args.output)
 
 def model(args):
-    if ((args.metabolite-mapping-complexes or args.nucleotide-conversions or args.adaptation or args.balance) and not args.metabolite2carbon):
+    if ((args.metabolitemappingcomplexes or args.nucleotideconversions or args.adaptation or args.balance) and not args.metabolite2carbon):
         raise RuntimeError("Must supply -d argument as well. Look at the help documentation.")
 
     #Import the metabolic reconstruction file name
@@ -25,22 +25,22 @@ def model(args):
     #Import other arguments and change name of exported model file
     #Prepend the code
     #test_model = model_file[:-4]
-    test_model = ''
-    if args.lower-bound:
-        test_model = test_mdoel + 'l'
-    if args.upper-bound:
-        test_model = test_model + 'u'
+    model_desc = ''
+    if args.lowerbound:
+        model_desc = model_desc + 'l'
+    if args.upperbound:
+        model_desc = model_desc + 'u'
     if args.gene2rxn:
-        test_model = test_model + 'g'
-    if args.metabolite-mapping-complexes:
-        test_model = test_model + 'm'
-    if args.nucleotide-conversions:
-        test_model = test_model + 'n'
+        model_desc = model_desc + 'g'
+    if args.metabolitemappingcomplexes:
+        model_desc = model_desc + 'm'
+    if args.nucleotideconversions:
+        model_desc = model_desc + 'n'
     if args.balance:
-        test_model = test_model + 'c'
+        model_desc = model_desc + 'c'
     if args.adaptation:
-        test_model = test_model + 'mod'
-    test_model = test_model + args.model.name
+        model_desc = model_desc + 'mod'
+    model_desc = model_desc + args.model.name
 
     #Import the model
     #Could just be read_sbml_model(args.model)
@@ -50,7 +50,7 @@ def model(args):
     #    cobra_model = cobra.io.mat.load_matlab_model(args.model)
 
     ##Make the changes to the model
-    model = model_class.set_parameter(args.model, args.sbml, args.cobra, args.extracellular, args.lower-bound, args.upper-boound, args.gene2rxn)
+    model = model_class.set_parameter(args.model, args.sbml, args.cobra, args.extracellular, args.lowerbound, args.upperbound, args.gene2rxn, model_desc)
     
     #model = model.metabolite_mapping(model, args_m)
     #model = model.nucleotide_conversion(model, args_n)
@@ -86,7 +86,7 @@ def main():
     parser_model.add_argument("biomassRxn", type=str,
                              help="Necessary variable: biomass rxn; Append 'R_' to the front if the reaction name does not begin with this.")
 ###Need to throw in a check here to make sure that the biomass rxn follows the naming convention.
-    parser_model.add_argument("externalcompartmentname", type=str,
+    parser_model.add_argument("extracellular", type=str,
                              help="Necessary variable: extracellular compartment abbreviation. Instead of brackets, use underscores (ex: '_e').)")
     parser_model.add_argument("-o", "--output", type=argparse.FileType("w"),
                               help="Name of model to be written out")
@@ -94,17 +94,17 @@ def main():
                              help='Flag to specify whether model is a COBRA Toolbox (.mat) file type; must have either -xml or -mat flag.')
     model_group.add_argument("-s", "--sbml", action="store_true",
                              help='Flag to specify whether model is a SBML (.xml) file type; must have either -xml or -mat flag.')
-    parser_model.add_argument("-l", "--lower-bound", type=argparse.FileType("r"), default=None,
+    parser_model.add_argument("-l", "--lowerbound", type=argparse.FileType("r"), default=None,
                               help="'lower boundary constraints file")
-    parser_model.add_argument("-u", "--uppper-bound", type=argparse.FileType("r"), default=None,
+    parser_model.add_argument("-u", "--upperbound", type=argparse.FileType("r"), default=None,
                               help="upper boundary constraints file")
     parser_model.add_argument("-g", "--gene2rxn", type=argparse.FileType("r"), default=None,
                               help="'gene2rxn file'")
     parser_model.add_argument("-d", "--metabolite2carbon", type=argparse.FileType("r"), default=None,
                               help="Tab-delimited file to specify dicitonary mappings of number of carbons in every metabolite. This is to check whether the model is carbon balanced. See iMM904 example for documentation.")
-    parser_model.add_argument("-m", "--metabolite-mapping-complexes", type=argparse.FileType("r"), default=None,
+    parser_model.add_argument("-m", "--metabolitemappingcomplexes", type=argparse.FileType("r"), default=None,
                               help="Tab-delimited metabolite mapping complexes file. See iMM904 example for documentation. Make sure model is carbon balanced if you use this; must have -d argument as well to use this.")
-    parser_model.add_argument("-n", "--nucleotide-conversions", type=argparse.FileType("r"), default=None,
+    parser_model.add_argument("-n", "--nucleotideconversions", type=argparse.FileType("r"), default=None,
                               help="Tab-delimited nucleotide conversions file. See iMM904 example for documentation. Make sure model is carbon balanced if you use this; must have -d argument as well to use this.")
     parser_model.add_argument("-a", "--adaptation",  type=argparse.FileType("r"), default=None, 
                               help='Tab delimited file to change specific reaction stoichiometries or to remove metabolites from reactions. See iMM904 example for documentation; must have -d argument as well to use this.') 
--- EXAMOARC/EXAMOARC/model.py
+++ EXAMOARC/EXAMOARC/model.py
@@ -3,7 +3,7 @@ import cobra
 import scipy as sp
 import numpy as np
 
-def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
+def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g, model_desc):
 	#Import the model
 	if args_s:
 		cobra_model = cobra.io.read_sbml_model(cobra_model)
@@ -13,6 +13,7 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 	idRs = []
 	lb = []
 	ub = []
+	genes_genes2rxn = []
 	pathway = []
 	rxn2lb = {}
 	rxn2ub = {}
@@ -43,7 +44,7 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 	#Import lower boundary adjustments if the argument is supplied from the command line. 
 	if args_l:
 		for x in args_l:
-			lb.append(float(x.strip))
+			lb.append(float(x))
 		for i, item1 in enumerate(cobra_model.reactions):
 			#reaction_name = re.sub("LPAREN","",str(item1))
 			#reaction_name = re.sub("RPAREN","",reaction_name)
@@ -53,13 +54,13 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 			#reaction_name = re.sub("\]","_",reaction_name)
 			#reaction_name = re.sub("\-","_",reaction_name)
 			#reaction_name = re.sub("__","_",reaction_name)
-			reaction_name = str("R_")+item1
+			reaction_name = 'R_'+str(item1)
 			rxn2lb[reaction_name] = lb[i]
 
 	#Import upper boundary adjustments if the argument is supplied from the command line. 
 	if args_u:
 		for x in args_u:
-			ub.append(float(x.strip))
+			ub.append(float(x))
 		for i, item1 in enumerate(cobra_model.reactions):
 			#reaction_name = re.sub("LPAREN","",str(item1))
 			#reaction_name = re.sub("RPAREN","",reaction_name)
@@ -69,13 +70,13 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 			#reaction_name = re.sub("\]","_",reaction_name)
 			#reaction_name = re.sub("\-","_",reaction_name)
 			#reaction_name = re.sub("__","_",reaction_name)
-			reaction_name = str("R_")+item1
+			reaction_name = 'R_'+str(item1)
 			rxn2ub[reaction_name] = ub[i]
 
 	#Import gene rule adjustments if the argument is supplied from the command line. 
 	if args_g:
 		for x in args_g:
-			genes_genes2rxn.append(x.strip)
+			genes_genes2rxn.append(x)
 		for i, item1 in enumerate(cobra_model.reactions):
 			#reaction_name = re.sub("LPAREN","",str(item1))
 			#reaction_name = re.sub("RPAREN","",reaction_name)
@@ -85,7 +86,7 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 			#reaction_name = re.sub("\]","_",reaction_name)
 			#reaction_name = re.sub("\-","_",reaction_name)
 			#reaction_name = re.sub("__","_",reaction_name)
-			reaction_name = str("R_")+item1
+			reaction_name = 'R_'+str(item1)
 			gene2rxn[reaction_name] = genes_genes2rxn[i]
 
 	#Create the necesssary rxn dictionaries for EXAMO.
@@ -99,19 +100,19 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 		#reaction_name = re.sub("\]","_",reaction_name)
 		#reaction_name = re.sub("\-","_",reaction_name)
 		#reaction_name = re.sub("__","_",reaction_name)
-		reaction_name = str("R_")+i.id
+		reaction_name = 'R_'+i.id
 		reactants = {}
 		reactants_original = {}
 		products = {}
 		products_original = {}
 		idRs.append(reaction_name)
-		if args_l:
+		if not args_l:
 			rxn2lb[reaction_name] = i.lower_bound
 			lb.append(float(i.lower_bound))
-		if args_u:
+		if not args_u:
 			rxn2ub[reaction_name] = i.upper_bound
 			ub.append(float(i.upper_bound))
-		if args_g:
+		if not args_g:
 			gene2rxn[reaction_name] = i.gene_reaction_rule
 		pathway.append(i.subsystem)
 		#Now need rxn
@@ -125,8 +126,8 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 				#metabolite_name = re.sub("\]","_",metabolite_name)
 				#metabolite_name = re.sub("\-","_",metabolite_name)
 				#metabolite_name = re.sub("__","_",metabolite_name)
-				reactants[str("M_")+j.id] = -1*i.metabolites[j]
-				reactants_original[str("M_")+j.id] = -1*i.metabolites[j]
+				reactants['M_'+j.id] = -1*i.metabolites[j]
+				reactants_original['M_'+j.id] = -1*i.metabolites[j]
 			if i.metabolites[j] > 0:
 				#metabolite_name = re.sub("LPAREN","",str(j.id))
 				#metabolite_name = re.sub("RPAREN","",metabolite_name)
@@ -136,21 +137,21 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 				#metabolite_name = re.sub("\]","_",metabolite_name)
 				#metabolite_name = re.sub("\-","_",metabolite_name)
 				#metabolite_name = re.sub("__","_",metabolite_name)
-				products[str("M_")+j.id] = i.metabolites[j]
-				products_original[str("M_")+j.id] = i.metabolites[j]
+				products['M_'+j.id] = i.metabolites[j]
+				products_original['M_'+j.id] = i.metabolites[j]
 		if len(products) == 0:
 			for j in reactants:
 				extracellular_string = args_e + '\Z'
 				idSp_e = re.search(extracellular_string, j)
 				if idSp_e is not None:
 					if args_e[-1:] == '_':
-						products[j[:-2]+str("b_")] = reactants[j]
-						products_original[j[:-2]+str("b_")] = reactants[j]
-						b_met.append(j[:-2]+str("b_"))
+						products[j[:-2]+'b_'] = reactants[j]
+						products_original[j[:-2]+'b_'] = reactants[j]
+						b_met.append(j[:-2]+'b_')
 					else:
-						products[j[:-1]+str("b")] = reactants[j]
-						products_original[j[:-1]+str("b")] = reactants[j]
-						b_met.append(j[:-1]+str("b"))
+						products[j[:-1]+'b'] = reactants[j]
+						products_original[j[:-1]+'b'] = reactants[j]
+						b_met.append(j[:-1]+'b')
 		if (rxn2lb[reaction_name] < 0 and rxn2ub[reaction_name] > 0):
 			reversible = True
 		else:
@@ -159,13 +160,13 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 		rxns_original[reaction_name] = {'name': i.name, 'id': reaction_name, 'reactants': reactants_original, 'products': products_original, 'reversible': reversible, 'genes': gene2rxn[reaction_name], 'lb': rxn2lb[reaction_name], 'ub': rxn2ub[reaction_name], 'pathway': i.subsystem}  
 
 	#Create list/sets/matlab cells of genes using cobrapy and array based model for reacitons
-	genes_cobra = cobra.io.mat._cell(cobra_model.genes.list_attr("id"))
-	grRules = cobra.io.mat._cell(cobra_model.reactions.list_attr("gene_reaction_rule"))
-	c = np.array(cobra_model.reactions.list_attr("objective_coefficient")) * 1
-	subsystem = cobra.io.mat._cell(cobra_model.reactions.list_attr("subsystem"))
-	metNames = cobra.io.mat._cell(cobra_model.metabolites.list_attr("name"))
+	genes_cobra = cobra.io.mat._cell(cobra_model.genes.list_attr('id'))
+	grRules = cobra.io.mat._cell(cobra_model.reactions.list_attr('gene_reaction_rule'))
+	c = np.array(cobra_model.reactions.list_attr('objective_coefficient')) * 1
+	subsystem = cobra.io.mat._cell(cobra_model.reactions.list_attr('subsystem'))
+	metNames = cobra.io.mat._cell(cobra_model.metabolites.list_attr('name'))
 	metFormulas = cobra.io.mat._cell([str(m.formula) for m in cobra_model.metabolites])
-	b = np.array(cobra_model.metabolites.list_attr("_bound")) * 1.
+	b = np.array(cobra_model.metabolites.list_attr('_bound')) * 1.
 
 	#Create gene set (for EXAMO model) and list (for COBRA model)
 	genes = set()
@@ -191,8 +192,8 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 		genes_list_dict[j] = i
 
 	#Create reaction gene matrix and rules object
-	rxnGeneMat = csr_matrix((len(idRs),len(genes)))
-	rxnGeneMat = lil_matrix(rxnGeneMat)
+	rxnGeneMat = sp.sparse.csr_matrix((len(idRs),len(genes)))
+	rxnGeneMat = sp.sparse.lil_matrix(rxnGeneMat)
 	rxn_index = 0
 	rules = grRules.copy()
 	for i in range(0,len(cobra_model.reactions)):
@@ -206,9 +207,9 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 			if genes_search is not None: 
 				rxn_index_0.append(rxn_index - 1)
 				gene_index_0.append(gene_index - 1)
-			rules[i] = re.sub(j,"x(%s)" % genes_list_dict[j],str(rules[i]))
-			rules[i] = re.sub("or","|",str(rules[i]))
-			rules[i] = re.sub("and","&",str(rules[i]))
+			rules[i] = re.sub(j,'x(%s)' % genes_list_dict[j],str(rules[i]))
+			rules[i] = re.sub('or','|',str(rules[i]))
+			rules[i] = re.sub('and','&',str(rules[i]))
 		rxnGeneMat[rxn_index_0, gene_index_0] = 1
 
 	#Map lb and ub to rxns based on the order in which they appear
@@ -285,7 +286,7 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 			del gene2rxn[i]
 			idRs.remove(i)
 
-		S = lil_matrix(csr_matrix(S)[:,rxn_index_list])
+		S = sp.sparse.lil_matrix(sp.sparse.csr_matrix(S)[:,rxn_index_list])
 
 		last_string_list = []
 		for i in idSp:
@@ -322,7 +323,7 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 			met_index_list.append(met_index - 1)
 
 
-	S = lil_matrix(csr_matrix(S)[met_index_list,:])
+	S = sp.sparse.lil_matrix(sp.sparse.csr_matrix(S)[met_index_list,:])
 
 	met_index = 0
 	met_index_to_delete = []
@@ -394,11 +395,11 @@ def set_parameter(cobra_model, args_s, args_c, args_e, args_l, args_u, args_g):
 	b = b[np.newaxis].T
 
 	#Convert the matrices into coo matrices
-	S = coo_matrix(S)
+	S = sp.sparse.coo_matrix(S)
 
-	rxnGeneMat = coo_matrix(rxnGeneMat)
+	rxnGeneMat = sp.sparse.coo_matrix(rxnGeneMat)
 
-	model = {'rxns': rxns_matlab, 'mets': mets_matlab, 'ub': ub_matlab, 'lb': lb_matlab, 'S': S, 'grRules': grRules, 'rules': rules, 'genes': genes_matlab, 'rxnGeneMat': rxnGeneMat, 'rev': rev_cobra, 'c': c, 'subsystem': subsystem, 'metNames': metNames, 'metFormulas': metFormulas, 'b': b, 'description': test_model[:-4]}
+	model = {'rxns': rxns_matlab, 'mets': mets_matlab, 'ub': ub_matlab, 'lb': lb_matlab, 'S': S, 'grRules': grRules, 'rules': rules, 'genes': genes_matlab, 'rxnGeneMat': rxnGeneMat, 'rev': rev_cobra, 'c': c, 'subsystem': subsystem, 'metNames': metNames, 'metFormulas': metFormulas, 'b': b, 'description': model_desc}
 
 	#Print all of the reactions in the final model
 	#for t in rxns:
