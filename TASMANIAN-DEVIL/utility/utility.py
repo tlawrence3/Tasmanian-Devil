caa0ef4d9990ea62e80f607530e13264eb8fc29c
Travis Lawrence <tlawrence3@ucmerced.edu>
2018-04-10 15:27:58 -0700

    fixed setup.py

--- TASMANIAN-DEVIL/utility/utility.py
+++
@@ -1,6 +0,0 @@
-#exportPickle function from EXAMO package
-def exportPickle(obj, fileName, mode = 'wb', protocol = -1):
-    import pickle
-    f = open(fileName, mode)
-    pickle.dump(obj, f, protocol = -1)
-    f.close()
--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 #Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
  
-setup(name = "TASMANIAN-DEVIL",
+setup(name = "TASMANIAN_DEVIL",
       setup_requires=['numpy', 'pandas'],
-      install_requires=['numpy', 'scipy', 'pandas', 'patsy', 'cobrapy==0.8.2', 'gurobipy'],
-      packages = ["TASMANIAN-DEVIL"],
-      entry_points = {"console_scripts": ['tas = TASMANIAN-DEVIL.TASMANIAN-DEVIL:main']},
+      install_requires=['numpy', 'scipy', 'pandas', 'patsy', 'cobra==0.8.2'],
+      packages = ["TASMANIAN_DEVIL"],
+      entry_points = {"console_scripts": ['tas = TASMANIAN_DEVIL.TASMANIAN_DEVIL:main']},
       version = "0.1.0",
-      description = "Something Something TASMANIAN-DEVIL",
+      description = "Something Something TASMANIAN_DEVIL",
       long_description=long_description,
       license='GPLv3',
       url = "www.nowhere.com",)
