f7557e414ddb84a275693043c8cef3c2dee85881
Travis Lawrence <tlawrence3@ucmerced.edu>
2018-04-25 11:33:18 -0700

    Update TASMANIAN_DEVIL.py

--- TASMANIAN_DEVIL/TASMANIAN_DEVIL.py
+++ TASMANIAN_DEVIL/TASMANIAN_DEVIL.py
@@ -523,7 +523,7 @@ def visualization(args):
     
 
 def main():
-    parser = argparse.ArgumentParser(prog="TASMANIAN-DEVIL")
+    parser = argparse.ArgumentParser(prog="tas")
     subparsers = parser.add_subparsers(help="sub-command help")
     
     # model subcommand parser
